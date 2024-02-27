0bfa5e8c341b9fbd68b5c70895c2c5b330c83a0f
Devyash Saini <dysaini2004@gmail.com>
2024-02-27 15:01:35 +0530

    stack

--- Lab 2.0/ImplementStack.java
+++ Lab 2.0/ImplementStack.java
@@ -58,5 +58,16 @@ class Stack<T> {
 }
 
 public class ImplementStack {
-    
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
+    }
 }
